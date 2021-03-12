b46583722f0b74fa954bc5e9ba6f4d22b42444b2
Yazdan Ranjbar <yazdan_ra@icloud.com>
2021-03-12 20:13:47 +0330

    uncomment `collectstatic`
    Signed-off-by: Yazdan Ranjbar <Yazdan_ra@icloud.com>

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -20,19 +20,21 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
-#python manage.py collectstatic --no-input
-#status=$?
-#if [ $status -ne 0 ]; then
-#  echo "Failed to collect staticfiles: $status"
-#  exit $status
-#fi
+python manage.py collectstatic --no-input
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to collect staticfiles: $status"
+  exit $status
+fi
+
 gunicorn boilerplate.wsgi:application \
         --name boilerplate-gunicorn \
         --bind 0.0.0.0:8000 \
         --workers 10 \
         --pythonpath "/opt/app/src" \
         --log-level=debug \
-        --timeout 120
+        --timeout 120 \
+        --reload
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start gunicorn: $status"
