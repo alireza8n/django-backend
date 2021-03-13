ffc66aaee9ffab8b652762871c6d78903064bd02
Yazdan Ranjbar <yazdan_ra@icloud.com>
2021-03-14 00:37:21 +0330

    log
    Signed-off-by: Yazdan Ranjbar <Yazdan_ra@icloud.com>

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -2,30 +2,36 @@
 
 set -e
 
-echo "Waiting for database..."
-while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
-echo "Connected to database."
+echo "DB_HOST" ${DB_HOST}
+echo "DB_PORT" ${DB_PORT}
+echo "DB_NAME" ${DB_NAME}
+echo "DB_USER" ${DB_USER}
+echo "DB_PASS" ${DB_PASS}
 
-python manage.py migrate --no-input
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to migrate database: $status"
-  exit $status
-fi
+#echo "Waiting for database..."
+#while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
+#echo "Connected to database."
 
-python manage.py compilemessages
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to compilemessages: $status"
-  exit $status
-fi
+#python manage.py migrate --no-input
+#status=$?
+#if [ $status -ne 0 ]; then
+#  echo "Failed to migrate database: $status"
+#  exit $status
+#fi
 
-python manage.py collectstatic --no-input
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to collect staticfiles: $status"
-  exit $status
-fi
+#python manage.py compilemessages
+#status=$?
+#if [ $status -ne 0 ]; then
+#  echo "Failed to compilemessages: $status"
+#  exit $status
+#fi
+#
+#python manage.py collectstatic --no-input
+#status=$?
+#if [ $status -ne 0 ]; then
+#  echo "Failed to collect staticfiles: $status"
+#  exit $status
+#fi
 
 gunicorn boilerplate.wsgi:application \
         --name boilerplate-gunicorn \
