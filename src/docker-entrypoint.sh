5299f9cf49505ba1ba09b1442e056ff352974c74
Yazdan Ranjbar <yazdan_ra@icloud.com>
2021-03-20 19:39:14 +0330

    Update entrypoint
    Signed-off-by: Yazdan Ranjbar <Yazdan_ra@icloud.com>

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -6,37 +6,46 @@ echo "Waiting for database..."
 while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
 echo "Connected to database."
 
-python manage.py migrate --no-input
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to migrate database: $status"
-  exit $status
-fi
+if [[ $# -gt 0 ]]; then
 
-python manage.py compilemessages
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to compilemessages: $status"
-  exit $status
-fi
+  INPUT=$@
+  sh -c "$INPUT"
 
-python manage.py collectstatic --no-input
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to collect staticfiles: $status"
-  exit $status
-fi
+else
 
-gunicorn boilerplate.wsgi:application \
-        --name boilerplate-gunicorn \
-        --bind 0.0.0.0:8000 \
-        --workers 10 \
-        --pythonpath "/opt/app/src" \
-        --log-level=debug \
-        --timeout 120 \
-        --reload
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start gunicorn: $status"
-  exit $status
+  python manage.py migrate --no-input
+  status=$?
+  if [ $status -ne 0 ]; then
+    echo "Failed to migrate database: $status"
+    exit $status
+  fi
+
+  python manage.py compilemessages
+  status=$?
+  if [ $status -ne 0 ]; then
+    echo "Failed to compilemessages: $status"
+    exit $status
+  fi
+
+  python manage.py collectstatic --no-input
+  status=$?
+  if [ $status -ne 0 ]; then
+    echo "Failed to collect staticfiles: $status"
+    exit $status
+  fi
+
+  gunicorn boilerplate.wsgi:application \
+    --name boilerplate-gunicorn \
+    --bind 0.0.0.0:8000 \
+    --workers 10 \
+    --pythonpath "/opt/app/src" \
+    --log-level=debug \
+    --timeout 120 \
+    --reload
+  status=$?
+  if [ $status -ne 0 ]; then
+    echo "Failed to start gunicorn: $status"
+    exit $status
+  fi
+
 fi
